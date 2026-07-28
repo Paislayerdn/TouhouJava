68c1f37c1bc107340b27b44f44ee54dc56b793ba
vongzathornnn <paislayerd@gmail.com>
2026-07-29 03:03:58 +0700

    icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,15 @@
 package main;
 
 import javax.swing.JFrame;
+import resource.ResourceLoader;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Touhou Java");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
+		window.setIconImage(ResourceLoader.image("Icon"));
+		
 
 		GamePanel panel = new GamePanel();
 		window.add(panel);
--- src/resource/ResourceFinder.java
+++ src/resource/ResourceFinder.java
@@ -2,15 +2,6 @@ package resource;
 
 import java.net.URL;
 
-final class ResourcePath {
-	private ResourcePath() {}
-
-	public static final String IMAGE = "/assets/images/";
-	public static final String SOUND = "/assets/sounds/";
-	public static final String MUSIC = "/assets/music/";
-	public static final String FONT  = "/assets/fonts/";
-}
-
 public final class ResourceFinder {
 	private ResourceFinder() {}
 
